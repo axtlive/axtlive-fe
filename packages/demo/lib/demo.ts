120be3e7d4f9afb613c5e668c51503a33b4721a9
Axtlive <axtlive@qq.com>
2024-03-21 00:07:04 +0800

    test

--- packages/demo/lib/demo.ts
+++ packages/demo/lib/demo.ts
@@ -1,3 +1,5 @@
+import utils from '@axtlive-fe/utils'
+
 export default function demo() {
   return 'Hello from demo';
 }
--- packages/utils/lib/index.ts
+++ packages/utils/lib/index.ts
@@ -1,5 +1,22 @@
 function utils() {
-  return 'Hello from utils';
+  return "Hello from utils";
+}
+
+function generateRandomString(length = 8): string {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+export {
+  generateRandomString
 }
 
 export default utils;
